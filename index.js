7a3d71bc0bc6a105faaf785be4dc94161892893a
Paul <pcherian10@gmail.com>
2018-04-20 16:28:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,20 +1,17 @@
 function add (a, b) {
   return a + b;
-  
 }
 
 function subtract (a, b) {
   return a - b;
-  
 }
 
 function multiply (a, b) {
   return a * b;
-  
 }
 
-function divide () {
-  
+function divide (a, b) {
+  return a / b;
 }
 
 function inc(n) {
